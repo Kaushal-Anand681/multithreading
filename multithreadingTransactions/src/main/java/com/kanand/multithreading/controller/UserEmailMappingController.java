11a0305b6786776bdb7f75d9420658dbce18aaec
JaiHo <JaiHo@DESKTOP-A8PUUFA>
2022-03-14 21:47:05 +0530

    added controller and service

--- multithreadingTransactions/src/main/java/com/kanand/multithreading/controller/UserEmailMappingController.java
+++ multithreadingTransactions/src/main/java/com/kanand/multithreading/controller/UserEmailMappingController.java
@@ -1,8 +1,24 @@
 package com.kanand.multithreading.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.kanand.multithreading.service.UserEmailMappingService;
+
+@RestController
 public class UserEmailMappingController {
+	
+	@Autowired
+	private UserEmailMappingService userEmailMappingService;
+	
+	@PostMapping(value = "/getUserEmail", produces = "application/json")
+	public ResponseEntity<Object> getUserEmailByName(@PathVariable(value="name") String name ) {
+		String email = userEmailMappingService.getUserEmailByName(name);
+		return new ResponseEntity<>(email, HttpStatus.OK);
+	}
 
 }
--- multithreadingTransactions/src/main/java/com/kanand/multithreading/service/UserEmailMappingService.java
+++ multithreadingTransactions/src/main/java/com/kanand/multithreading/service/UserEmailMappingService.java
@@ -1,6 +1,7 @@
 package com.kanand.multithreading.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Service;
 
 import com.kanand.multithreading.repository.IUserEmailMappingRepository;
@@ -9,5 +10,17 @@ import com.kanand.multithreading.repository.IUserEmailMappingRepository;
 public class UserEmailMappingService {
 	
 	@Autowired
+	private TaskExecutor taskExecutor;
+	
+	@Autowired
 	private IUserEmailMappingRepository userEmailMappingRepository;
+
+	public String getUserEmailByName(String name) {
+		
+		Runnable r = () -> {
+			
+		};
+		taskExecutor.execute(r);
+		return "Done";
+	}
 }
