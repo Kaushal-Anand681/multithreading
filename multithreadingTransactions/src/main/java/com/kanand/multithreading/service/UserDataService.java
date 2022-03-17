c23ea9f7de1c614b273cc4574b53214211ecd66e
JaiHo <JaiHo@DESKTOP-A8PUUFA>
2022-03-17 22:03:10 +0530

    url connection check method

--- multithreadingTransactions/src/main/java/com/kanand/multithreading/service/UserDataService.java
+++ multithreadingTransactions/src/main/java/com/kanand/multithreading/service/UserDataService.java
@@ -1,5 +1,10 @@
 package com.kanand.multithreading.service;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Service;
@@ -27,4 +32,25 @@ public class UserDataService {
 		taskExecutor.execute(r);
 		return "InProgress";
 	}
+	
+	public boolean checkUrl(String urlString) {
+		URLConnection connection = null;
+		try {
+			URL url = new URL(urlString);
+			connection = url.openConnection();
+			if(connection != null) {
+				connection.connect();
+			}else {
+				return false;
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return true;
+	}
+	
 }
